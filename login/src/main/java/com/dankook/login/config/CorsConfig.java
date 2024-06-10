e8ae22b771f5d20788098d3164dbe1d164d88035
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-06-10 20:58:13 +0900

    fix: test jenkins

--- login/src/main/java/com/dankook/login/config/CorsConfig.java
+++ login/src/main/java/com/dankook/login/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://react-service.react.svc.cluster.local:3000")
+                .allowedOrigins("http://localhost:3000", "http://react-service.react.svc.cluster.local:3000", "http://172.16.10.175:3000")
                 .exposedHeaders("*")
                 .allowCredentials(true);
     }
